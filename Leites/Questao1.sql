b316ac3688908f90531feb9ba08764ccf65674ce
Iury Barbosa <iury.alves@ccc.ufcg.edu.br>
2024-05-08 03:57:08 -0300

    [ADD] Leite - Questao 1 edit

--- Leites/Questao1.sql
+++ Leites/Questao1.sql
@@ -6,6 +6,12 @@ Mostre os nomes e valor do estoque daqueles produtos que tiveram transações re
 e Compra com quantidade maior que 10.
 */
 
+SELECT p.NOME, p.PRECO
+FROM PRODUTO p, COMPRA com
+WHERE p.ID_PRODUTO = com.ID_PRODUTO AND com.QUANTIDADE > 10;
+
+-- VALOR TOTAL DO ESTOQUE 
+
 SELECT 
     p.NOME,
     SUM(p.PRECO * p.ESTOQUE) AS TOTAL_VALOR_ESTOQUE
